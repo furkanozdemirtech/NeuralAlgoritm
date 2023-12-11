c7703d8540c6c2449788057a107b682b1e568b0b
furkanozdemirtech <141388713+furkanozdemirtech@users.noreply.github.com>
2023-12-11 16:22:45 +0300

    Neural Agoritması
    Oneri platform ıcın gelistirilmistir

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_filmadı = new System.Windows.Forms.TextBox();
+            this.textBox_yili = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_tur = new System.Windows.Forms.TextBox();
+            this.textBox_puan = new System.Windows.Forms.TextBox();
+            this.textBox_yonetmen = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(602, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1033, 735);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox_filmadı
+            // 
+            this.textBox_filmadı.Location = new System.Drawing.Point(112, 12);
+            this.textBox_filmadı.Name = "textBox_filmadı";
+            this.textBox_filmadı.Size = new System.Drawing.Size(261, 22);
+            this.textBox_filmadı.TabIndex = 1;
+            // 
+            // textBox_yili
+            // 
+            this.textBox_yili.Location = new System.Drawing.Point(112, 47);
+            this.textBox_yili.Name = "textBox_yili";
+            this.textBox_yili.Size = new System.Drawing.Size(261, 22);
+            this.textBox_yili.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Yıl";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tür";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Puan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 154);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Yönetmen";
+            // 
+            // textBox_tur
+            // 
+            this.textBox_tur.Location = new System.Drawing.Point(112, 79);
+            this.textBox_tur.Name = "textBox_tur";
+            this.textBox_tur.Size = new System.Drawing.Size(261, 22);
+            this.textBox_tur.TabIndex = 9;
+            // 
+            // textBox_puan
+            // 
+            this.textBox_puan.Location = new System.Drawing.Point(112, 116);
+            this.textBox_puan.Name = "textBox_puan";
+            this.textBox_puan.Size = new System.Drawing.Size(261, 22);
+            this.textBox_puan.TabIndex = 10;
+            // 
+            // textBox_yonetmen
+            // 
+            this.textBox_yonetmen.Location = new System.Drawing.Point(112, 154);
+            this.textBox_yonetmen.Name = "textBox_yonetmen";
+            this.textBox_yonetmen.Size = new System.Drawing.Size(261, 22);
+            this.textBox_yonetmen.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1635, 735);
+            this.Controls.Add(this.textBox_yonetmen);
+            this.Controls.Add(this.textBox_puan);
+            this.Controls.Add(this.textBox_tur);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_yili);
+            this.Controls.Add(this.textBox_filmadı);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_filmadı;
+        private System.Windows.Forms.TextBox textBox_yili;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_tur;
+        private System.Windows.Forms.TextBox textBox_puan;
+        private System.Windows.Forms.TextBox textBox_yonetmen;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,303 @@
+﻿using Accord.Neuro;
+using Accord.Neuro.Learning;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static List<Movie> VerileriOku(string dosyaYolu)
+        {
+            List<Movie> filmler = new List<Movie>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(dosyaYolu))
+                {
+                    string baslikSatiri = sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        string satir = sr.ReadLine();
+                        string[] veri = satir.Split(',');
+                        Movie movie = new Movie
+                        {
+                            Img_Url = veri[0],
+                            Title = veri[1],
+                            Year = veri[2],
+                            Certificate = veri[3],
+                            Duration = veri[4],
+                            Genre = veri[5],
+                            Imdb = veri[6],
+                            Metascore = veri[7],
+                            Overview = veri[8],
+                            Director = veri[9],
+                            Actors = veri[10],
+                        };
+                        filmler.Add(movie);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return filmler;
+        }
+        static double[][] GetInputsFromMovies(List<Movie> movies)
+        {
+            return movies.Select(movie => new double[] { movie.Title.Length, movie.Genre.Length }).ToArray();
+        }
+
+        static double[] GetOutputsFromMovies(List<Movie> movies)
+        {
+
+            Random random = new Random();
+            return movies.Select(movie => random.NextDouble()).ToArray();
+        }
+        public double[] StringArrayToDoubleArray(string[] input)
+        {
+            return input.Select(c => (double)c[0]).ToArray();
+        }
+        static double GenerateRandomOutput()
+        {
+            Random random = new Random();
+            return random.NextDouble();
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string dosyaYolu = "/DataList/data.csv";
+            List<Movie> movies = new List<Movie>();
+            movies = VerileriOku(dosyaYolu);
+            double[][] inputs = movies.Select(movie => new double[] { movie.Duration.Length, movie.Genre.Length }).ToArray();
+            double[][] outputs = movies.Select(movie => new double[] { GenerateRandomOutput() }).ToArray();
+
+
+            ActivationNetwork network = new ActivationNetwork(
+                new SigmoidFunction(),
+                2,
+                2,
+                1
+                );
+            BackPropagationLearning teacher = new BackPropagationLearning(network);
+            int iterations = 1000;
+            double erororTolerance = 0.01;
+            double error = double.MaxValue;
+            List<string> erorlist = new List<string>();
+            for (int i = 0; i < iterations && error > erororTolerance; i++)
+            {
+                error = teacher.RunEpoch(inputs, outputs);
+                erorlist.Add($"Iteration {i + 1}, Error: {error}");
+
+            }
+            List<KeyValuePair<string, double>> topMovies = GetTopNOutputsFromMovies(movies, network, 5);
+
+            //string name = "john";
+            //string secim = "1";
+
+            //string apiUrl = $"http://127.0.0.1:5000/getdata?name={name}&secim={secim}";
+
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    try
+            //    {
+            //        HttpResponseMessage response = await client.GetAsync(apiUrl);
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            string responseBody = await response.Content.ReadAsStringAsync();
+            //            JsonData jsonData = JsonConvert.DeserializeObject<JsonData>(responseBody);
+            //            columnValues = new Dictionary<string, string>();
+            //            int top = 10;
+            //            foreach (var columnName in jsonData.Columns)
+            //            {
+            //                // Label'u form üzerine ekleyin
+            //                Label label = new Label();
+            //                label.Text = columnName + ":";
+            //                label.Location = new System.Drawing.Point(10, top);
+            //                label.Size = new System.Drawing.Size(100, 20);
+            //                this.Controls.Add(label);
+
+            //                // TextBox'u form üzerine ekleyin
+            //                TextBox textBox = new TextBox();
+            //                textBox.Location = new System.Drawing.Point(120, top);
+            //                textBox.Size = new System.Drawing.Size(200, 20);
+            //                textBox.Tag = columnName; // TextBox'ın hangi özelliği temsil ettiğini belirtmek için Tag özelliğini kullanıyoruz
+            //                this.Controls.Add(textBox);
+            //                top += 30;
+            //                columnValues.Add(columnName, textBox.Text);
+            //            }
+            //            Button submitButton = new Button();
+            //            submitButton.Location = new System.Drawing.Point(10, top);
+            //            submitButton.Size = new System.Drawing.Size(75, 23);
+            //            submitButton.Text = "Gönder";
+            //            submitButton.Click += SubmitButton_Click;
+            //            this.Controls.Add(submitButton);
+            //            var tableData = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonData>(responseBody);
+            //            foreach (var column in tableData.Columns)
+            //            {
+
+            //            }
+            //            List<string> list = new List<string>();
+
+            //            MessageBox.Show("Ulaşmak istegimiz string değer" + responseBody);
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("İşleminiz Gerçekleştirilemi" + response.ReasonPhrase);
+            //        }
+            //    }
+            //    catch (HttpRequestException a)
+            //    {
+            //        string mesaj = a.Message;
+            //        throw;
+            //    }
+            //}
+        }
+
+
+
+        public class DataRsponse
+        {
+            public IList<string> Columns { get; set; }
+        }
+        Dictionary<string, object> row = new Dictionary<string, object>
+        {
+            { "Feature1", "Value1" },
+            { "Feature2", "Value2" }
+        };
+        List<string> selectedFeatures = new List<string> { "Feature1", "Feature2" };
+        public string CreateCombinedFeatures(Dictionary<string, object> row, List<string> selectedFeatures)
+        {
+
+            string result = string.Join(" ", selectedFeatures.Select(feature => row[feature]));
+
+            return result;
+        }
+        public class JsonData
+        {
+            public string[] Columns { get; set; }
+        }
+        public Dictionary<string, string> columnValues;
+        public class RecommendationResponse
+        {
+            public List<List<string>> Recommendations { get; set; }
+            public List<double> Scores { get; set; }
+        }
+        public Dictionary<string, string> Constract_String()
+        {
+            Dictionary<string, string> columnValues = new Dictionary<string, string>();
+
+            foreach (Control control in this.Controls)
+            {
+                if (control is TextBox textBox && textBox.Tag != null)
+                {
+                    // Her bir TextBox için Tag özelliği kullanarak değeri ekleyin
+                    columnValues.Add(textBox.Tag.ToString(), textBox.Text);
+                }
+            }
+
+            // Önceki değerleri temizleme (isteğe bağlı)
+            // columnValues.Clear();
+
+            return columnValues;
+        }
+        public class Movie
+        {
+            public string Title { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Year { get; set; }
+            public string Img_Url { get; set; }
+            public string Certificate { get; set; }
+            public string Duration { get; set; }
+            public string Imdb { get; set; }
+            public string Metascore { get; set; }
+            public string Overview { get; set; }
+            public string Actors { get; set; }
+            public double Scores { get; set; }
+            public string[] StringArrayData { get; set; }
+        }
+
+        private async void SubmitButton_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        public class MovieOutput
+        {
+            public string Title { get; set; }
+            public double Output { get; set; }
+        }
+        static List<KeyValuePair<string, double>> GetTopNOutputsFromMovies(List<Movie> movies, ActivationNetwork network, int n)
+        {
+            List<KeyValuePair<string, double>> outputs = new List<KeyValuePair<string, double>>();
+            foreach (var movie in movies)
+            {
+                double output = network.Compute(new double[] { movie.Duration.Length, movie.Genre.Length })[0];
+                outputs.Add(new KeyValuePair<string, double>(movie.Duration, output));
+            }
+            outputs.Sort((x, y) => y.Value.CompareTo(y.Value));
+            List<KeyValuePair<string, double>> topNOutputs = outputs.Take(n).ToList();
+            return topNOutputs;
+        }
+        static List<MovieOutput> GetOutputsFromMoviesRandom(List<Movie> movies, int n)
+        {
+            Random random = new Random();
+
+            var outputs = new List<MovieOutput>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var movie = movies[random.Next(movies.Count)];
+                outputs.Add(new MovieOutput { Title = movie.Title, Output = random.NextDouble() });
+            }
+
+            return outputs;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string title = textBox_filmadı.Text.Trim();
+                string year = textBox_yili.Text.Trim().Trim();
+                string genre = textBox_tur.Text.Trim();
+                string imdb = textBox_puan.Text.Trim();
+                string director = textBox_yonetmen.Text.Trim();
+                string apiUrl = $"http://localhost:5000/get_recommendations?title={title}&year={year}&genre={genre}&imdb={imdb}&director={director}";
+                columnValues = Constract_String();
+                columnValues = columnValues.Where(p => !string.IsNullOrEmpty(p.Value)).ToDictionary(p => p.Key, p => p.Value);
+                string requestUrl = $"{apiUrl}?&{string.Join("&", columnValues.Select(p => $"{p.Key}={p.Value}"))}";
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var recommendations = JsonConvert.DeserializeObject<RecommendationResponse>(responseData);
+                    var list_movie = new List<Movie>();
+                    foreach (var recommendation in recommendations.Recommendations)
+                    {
+                        int i = 0;
+                        string[] lines = recommendation.ToArray();
+                        var node_movie = new Movie { Title = lines[0].ToString(), Director = lines[2].ToString(), Genre = lines[1].ToString(), Year = lines[3].ToString(), Img_Url = lines[4].ToString(), Certificate = lines[5].ToString(), Duration = lines[6].ToString(), Imdb = lines[7].ToString(), Metascore = lines[8], Overview = lines[9], Actors = lines[10], Scores = recommendations.Scores[i] };
+                        list_movie.Add(node_movie);
+                        i++;
+                    }
+                    dataGridView1.DataSource = list_movie.ToList();
+                }
+                else
+                {
+                    MessageBox.Show($"Hata kodu: {response.StatusCode}");
+                }
+            }
+        }
+    }
+}
